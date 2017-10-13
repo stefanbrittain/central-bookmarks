eacb41c49b07896f82d2735ea817a5dbe6c20457
stefan <stefan>
2017-10-13 09:07:04 +0000

    Added Jasmine test for About component

--- ui/src/app/about.component.ts
+++ ui/src/app/about.component.ts
@@ -3,11 +3,12 @@ import { AppAction} from './appAction';
 
 @Component({
     selector: 'about-dialog',
-    template: `<div class="popup"><button class="button" (click)="close()"><span>Close </span></button></div>`
+    template: `<div class="popup"><h1>{{test}}</h1><button class="button" (click)="close()"><span>Close </span></button></div>`
 })
 
 export class AboutComponent { //implements OnInit, OnDestroy {
   @Output() systemMessage: EventEmitter<any> = new EventEmitter();
+  test : string = "Jasmine Test String";
 
   close(){
     this.systemMessage.emit(new AppAction("About"));
